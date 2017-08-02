92e8c0ca397be3da65090a1e311371ea2440089f
David Johnson <davjohns@Boreas.local>
2017-08-01 20:27:27 -0700

    Initial commit

---
+++ Garage Door Opener/OpenTheDoor.swift
@@ -0,0 +1,10 @@
+//
+//  OpenTheDoor.swift
+//  Garage Door Opener
+//
+//  Created by David Johnson on 11/19/16.
+//  Copyright © 2016 David Johnson. All rights reserved.
+//
+
+import Foundation
+
--- Garage Door Opener/ViewController.swift
+++ Garage Door Opener/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: Properties
 
+    @IBOutlet weak var buttonOpenDoor: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,71 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // MARK: Actions
+    
+    @IBAction func openDoor(_ sender: UIButton) {
+        let endPointUrl = "http://edave.dyndns.org:5000"
+        let myUrl = NSURL(string: endPointUrl);
+        
+        // Creaste URL Request
+        let request = NSMutableURLRequest(url:myUrl! as URL);
+        
+        // Set request HTTP method to GET. It could be POST as well
+        request.httpMethod = "GET"
+        
+        let task = URLSession.shared.dataTask(with: request as URLRequest) {
+            data, response, error in
+            
+            // Check for error
+            if error != nil
+            {
+                print("error=\(error)")
+                return
+            }
+            
+            // Print out response string
+            let responseString = NSString(data: data!, encoding: String.Encoding.utf8.rawValue)
+            print("responseString = \(responseString)")
+        }
+        
+        task.resume()
+    }
+    // Return IP address of WiFi interface (en0) as a String, or `nil`
+    @IBAction func getWiFiAddress(_ sender: UIButton) {
+        var address : String?
+        
+        // Get list of all interfaces on the local machine:
+        var ifaddr : UnsafeMutablePointer<ifaddrs>?
+        guard getifaddrs(&ifaddr) == 0 else { return nil }
+        guard let firstAddr = ifaddr else { return nil }
+        
+        // For each interface ...
+        for ifptr in sequence(first: firstAddr, next: { $0.pointee.ifa_next }) {
+            let interface = ifptr.pointee
+            
+            // Check for IPv4 or IPv6 interface:
+            let addrFamily = interface.ifa_addr.pointee.sa_family
+            if addrFamily == UInt8(AF_INET) || addrFamily == UInt8(AF_INET6) {
+                
+                // Check interface name:
+                let name = String(cString: interface.ifa_name)
+                if  name == "en0" {
+                    
+                    // Convert interface address to a human readable string:
+                    var addr = interface.ifa_addr.pointee
+                    var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+                    getnameinfo(&addr, socklen_t(interface.ifa_addr.pointee.sa_len),
+                                &hostname, socklen_t(hostname.count),
+                                nil, socklen_t(0), NI_NUMERICHOST)
+                    address = String(cString: hostname)
+                }
+            }
+        }
+        freeifaddrs(ifaddr)
+        
+        return address
+        
+    }
 }
 
